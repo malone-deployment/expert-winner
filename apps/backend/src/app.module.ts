87d7de452b77d3ce74b0698fd5d57e21cc22475a
fretzestavillo <estavillofretz@gmail.com>
2024-11-20 12:05:16 +0800

    test

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -7,15 +7,14 @@ import { WifiModule } from './wifi/wifi.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '/cloudsql/rpi-hub-438905:asia-southeast1:postgresql',
+      host: process.env.INSTANCE_UNIX_SOCKET, // This will pull the socket path from environment variable
       port: 5432,
       extra: {
-        socketPath: '/cloudsql/rpi-hub-438905:asia-southeast1:postgresql',
+        socketPath: process.env.INSTANCE_UNIX_SOCKET, // Using the environment variable for socket path
       },
-      username:
-        'estavillofretz-developer-rpi-h@rpi-hub-438905.iam.gserviceaccount.com',
-      database: 'postgres',
-      password: '9DPx*:O=S3rDL.:l', // Use environment variable if available
+      username: process.env.DB_USER, // Use environment variable for DB user
+      database: process.env.DB_NAME, // Use environment variable for DB name
+      password: process.env.DB_PASS, // Use environment variable for DB password
       entities: [WifiEntity],
       synchronize: true,
     }),
