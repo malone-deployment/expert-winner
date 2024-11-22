6ebba0f387529de3099784dd30a9ffefbd10f172
fretzestavillo <estavillofretz@gmail.com>
2024-11-22 13:47:47 +0800

    test

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,16 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const HOST = '0.0.0.0';
-// 'https://backend-image-422041495987.asia-southeast1.run.app/wifi/ this is the right host base'
+const HOST =
+  'https://web-service-422041495987.asia-southeast1.run.app/wifi/fck';
 const PORT = Number(process.env.PORT) || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(PORT, HOST).then(() => {
-    // tslint:disable-next-line:no-console
     console.log(
-      `** Nest Live Development Server is listening on ${HOST}:${PORT}, open your browser on http://localhost:${PORT}/ **`,
+      `** Nest Live Development Server is listening on ${HOST}, open your browser on ${HOST}/ **`,
     );
   });
 }
