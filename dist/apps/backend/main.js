d7ea708fb78381c8e804cafa40d28ff123f41556
fretzestavillo <estavillofretz@gmail.com>
2024-11-25 10:42:38 +0800

    test

--- dist/apps/backend/main.js
+++ dist/apps/backend/main.js
@@ -19,53 +19,51 @@ const common_1 = __webpack_require__(4);
 const typeorm_1 = __webpack_require__(5);
 const wifi_entity_1 = __webpack_require__(6);
 const wifi_module_1 = __webpack_require__(8);
-const config_1 = __webpack_require__(15);
-const config_schema_1 = __webpack_require__(16);
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//       envFilePath: [`.env`],
+//       validationSchema: configValdidationSchema,
+//     }),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       inject: [ConfigService],
+//       useFactory: async (configService: ConfigService) => ({
+//         type: 'postgres',
+//         host: configService.get('PG_HOST'),
+//         port: configService.get('PG_PORT'),
+//         username: configService.get('PG_USERNAME'),
+//         database: configService.get('PG_DATABASE'),
+//         password: configService.get('PG_PASSWORD'),
+//         entities: [WifiEntity],
+//         synchronize: true,
+//       }),
+//     }),
+//     WifiModule,
+//   ],
+// })
+// export class AppModule {}
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = tslib_1.__decorate([
     (0, common_1.Module)({
         imports: [
-            config_1.ConfigModule.forRoot({
-                isGlobal: true,
-                envFilePath: [`.env`],
-                validationSchema: config_schema_1.configValdidationSchema,
-            }),
-            typeorm_1.TypeOrmModule.forRootAsync({
-                imports: [config_1.ConfigModule],
-                inject: [config_1.ConfigService],
-                useFactory: async (configService) => ({
-                    type: 'postgres',
-                    host: configService.get('PG_HOST'),
-                    port: configService.get('PG_PORT'),
-                    username: configService.get('PG_USERNAME'),
-                    database: configService.get('PG_DATABASE'),
-                    password: configService.get('PG_PASSWORD'),
-                    entities: [wifi_entity_1.WifiEntity],
-                    synchronize: true,
-                }),
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'postgres',
+                host: '/cloudsql/rpi-hub-438905:asia-southeast1:postgresql',
+                port: 5432,
+                username: 'postgres',
+                database: 'postgres',
+                password: '9DPx*:O=S3rDL.:l',
+                entities: [wifi_entity_1.WifiEntity],
+                synchronize: true,
             }),
             wifi_module_1.WifiModule,
         ],
     })
 ], AppModule);
-// @Module({
-//     imports: [
-//       TypeOrmModule.forRoot({
-//         type: 'postgres',
-//         host: '/cloudsql/rpi-hub-438905:asia-southeast1:postgresql',
-//         port: 5432,
-//         username: 'postgres',
-//         database: 'postgres',
-//         password: '9DPx*:O=S3rDL.:l',
-//         entities: [WifiEntity],
-//         synchronize: true,
-//       }),
-//       WifiModule,
-//     ],
-//   })
-//   export class AppModule {}
 
 
 /***/ }),
@@ -366,38 +364,6 @@ tslib_1.__decorate([
 /***/ ((module) => {
 
 module.exports = require("class-validator");
-
-/***/ }),
-/* 15 */
-/***/ ((module) => {
-
-module.exports = require("@nestjs/config");
-
-/***/ }),
-/* 16 */
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.configValdidationSchema = void 0;
-const tslib_1 = __webpack_require__(3);
-const Joi = tslib_1.__importStar(__webpack_require__(17));
-exports.configValdidationSchema = Joi.object({
-    //   STAGE: Joi.string().required(),
-    DB_HOST: Joi.string().required(),
-    DB_PORT: Joi.number().default(5432).required(),
-    DB_USERNAME: Joi.string().required(),
-    DB_PASSWORD: Joi.string().required(),
-    DB_DATABASE: Joi.string().required(),
-    //   JWT_SECRET: Joi.string().required(),
-});
-
-
-/***/ }),
-/* 17 */
-/***/ ((module) => {
-
-module.exports = require("joi");
 
 /***/ })
 /******/ 	]);
