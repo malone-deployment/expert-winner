243caae85745e5f0b105590f9ea9a05f6d4ea039
fretzestavillo <estavillofretz@gmail.com>
2024-11-26 12:45:45 +0800

    test

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WifiEntity } from './wifi/wifi.entity';
 import { WifiModule } from './wifi/wifi.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '/cloudsql/rpi-hub-438905:asia-southeast1:postgresql',
-      port: 5432,
-      username: 'postgres',
-      database: 'postgres',
-      password: '9DPx*:O=S3rDL.:l',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [WifiEntity],
       synchronize: true,
     }),
@@ -19,3 +23,25 @@ import { WifiModule } from './wifi/wifi.module';
   ],
 })
 export class AppModule {}
+
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { WifiEntity } from './wifi/wifi.entity';
+// import { WifiModule } from './wifi/wifi.module';
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: '/cloudsql/rpi-hub-438905:asia-southeast1:postgresql',
+//       port: 5432,
+//       username: 'postgres',
+//       database: 'postgres',
+//       password: '9DPx*:O=S3rDL.:l',
+//       entities: [WifiEntity],
+//       synchronize: true,
+//     }),
+//     WifiModule,
+//   ],
+// })
+// export class AppModule {}
--- dist/apps/backend/main.js
+++ dist/apps/backend/main.js
@@ -19,19 +19,23 @@ const common_1 = __webpack_require__(4);
 const typeorm_1 = __webpack_require__(5);
 const wifi_entity_1 = __webpack_require__(6);
 const wifi_module_1 = __webpack_require__(8);
+const config_1 = __webpack_require__(15);
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = tslib_1.__decorate([
     (0, common_1.Module)({
         imports: [
+            config_1.ConfigModule.forRoot({
+                isGlobal: true,
+            }),
             typeorm_1.TypeOrmModule.forRoot({
                 type: 'postgres',
-                host: '/cloudsql/rpi-hub-438905:asia-southeast1:postgresql',
-                port: 5432,
-                username: 'postgres',
-                database: 'postgres',
-                password: '9DPx*:O=S3rDL.:l',
+                host: process.env.DB_HOST,
+                port: parseInt(process.env.DB_PORT, 10),
+                username: process.env.DB_USER,
+                password: process.env.DB_PASS,
+                database: process.env.DB_NAME,
                 entities: [wifi_entity_1.WifiEntity],
                 synchronize: true,
             }),
@@ -39,6 +43,26 @@ exports.AppModule = AppModule = tslib_1.__decorate([
         ],
     })
 ], AppModule);
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { WifiEntity } from './wifi/wifi.entity';
+// import { WifiModule } from './wifi/wifi.module';
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: '/cloudsql/rpi-hub-438905:asia-southeast1:postgresql',
+//       port: 5432,
+//       username: 'postgres',
+//       database: 'postgres',
+//       password: '9DPx*:O=S3rDL.:l',
+//       entities: [WifiEntity],
+//       synchronize: true,
+//     }),
+//     WifiModule,
+//   ],
+// })
+// export class AppModule {}
 
 
 /***/ }),
@@ -339,6 +363,12 @@ tslib_1.__decorate([
 /***/ ((module) => {
 
 module.exports = require("class-validator");
+
+/***/ }),
+/* 15 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/config");
 
 /***/ })
 /******/ 	]);
